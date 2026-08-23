eb8933097c61c6a9f5e46f8b873ea60fdb6441a2
pro-till <tillageagrobaska@gmail.com>
2026-08-23 13:43:56 +0530

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,14 @@
+Gibbrex/
+├── index.html
+├── batches-data.js
+├── toxicity-symbol.png          ← only here
+├── leaflet.pdf                  ← only here
+│
+├── 2608WTG15-500ML/
+│   └── index.html
+├── 2608WTG16-1LTR/
+│   └── index.html
+├── 2608WTG17-250ML/
+│   └── index.html
+└── 2608WTG18-1LTR/
+    └── index.html
---
+++ batches-data.js
@@ -0,0 +1,46 @@
+// batches-data.js
+// ====================== THIS IS THE SINGLE SOURCE OF TRUTH ======================
+// Edit only this file when you add/change any batch.
+
+const batches = [
+  {
+    uid: "20010307500",
+    batchNo: "2608WTG15",
+    packSize: "500 ML",
+    mfgDate: "20-08-2026",
+    expDate: "19-08-2028",
+    mrp: "Rs. 620/-",
+    usp: "Rs. 1.24/ml",
+    folderName: "2608WTG15-500ML"
+  },
+  {
+    uid: "20010307001",
+    batchNo: "2608WTG16",
+    packSize: "1 LTR",
+    mfgDate: "10-10-2026",
+    expDate: "09-10-2028",
+    mrp: "Rs. 1200/-",
+    usp: "Rs. 1.20/ml",
+    folderName: "2608WTG16-1LTR"
+  },
+  {
+    uid: "20010307250",
+    batchNo: "2608WTG17",
+    packSize: "250 ML",
+    mfgDate: "10-10-2026",
+    expDate: "09-08-2028",
+    mrp: "Rs. 320/-",
+    usp: "Rs. 1.28/ml",
+    folderName: "2608WTG17-250ML"
+  },
+  {
+    uid: "20010307001",
+    batchNo: "2608WTG18",
+    packSize: "1 LTR",
+    mfgDate: "25-12-2026",
+    expDate: "24-12-2028",
+    mrp: "Rs. 1200/-",
+    usp: "Rs. 1.20/ml",
+    folderName: "2608WTG18-1LTR"
+  }
+];
